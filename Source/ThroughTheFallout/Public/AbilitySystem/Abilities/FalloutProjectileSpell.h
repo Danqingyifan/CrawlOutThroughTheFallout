963ce35073219cd4eb7a43aabbba29b6f427c161
Danqingyifan <danqingyifan@gmail.com>
2024-09-13 02:04:31 +0800

    update VFX asset

---
+++ Source/ThroughTheFallout/Public/AbilitySystem/Abilities/FalloutProjectileSpell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/FalloutGameplayAbility.h"
+#include "FalloutProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THROUGHTHEFALLOUT_API UFalloutProjectileSpell : public UFalloutGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
